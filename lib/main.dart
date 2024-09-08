927d71e849ff0ff04c0438af887a18e12b931c7b
Prohemant001 <prahemant001@gmail.com>
2024-09-08 17:21:46 +0530

    day9

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/main_home_screen.dart';
 
-void main(){
-
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: MainHomeScreen(),
+      ),
+    );
   }
 }

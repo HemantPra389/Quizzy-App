55a62c45cfea30afa42c27181b6e31b2d1105c1e
Prohemant001 <prahemant001@gmail.com>
2024-09-11 23:30:36 +0530

    day 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/screens/main_home_screen.dart';
+import 'package:quiz_app/screens/quiz_result_screen.dart';
 import 'package:quiz_app/screens/quiz_screen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(child: MainHomeScreen()),
+      home: SafeArea(child: QuizResultScreen()),
     );
   }
 }
